0c48d2118f24f444038e8576f869641034ea2bce
Jianxiang Wang (王健翔) <qrox@sina.com>
2019-09-21 17:51:52 +0800

    Update before-deploy.sh

--- before-deploy.sh
+++ before-deploy.sh
@@ -3,11 +3,20 @@
 set -uex
 
 name=clang-tidy-plugin-support-$TRAVIS_TAG
-mkdir -p $name/{include,bin,lib}
-cp clang/build/bin/clang-tidy $name/bin
-cp clang/tools/extra/test/clang-tidy/check_clang_tidy.py $name/bin
+mkdir -p $name/{bin,include,lib}
+# bin
+cp llvm-project/build/bin/clang-tidy $name/bin
+cp llvm-project/clang-tools-extra/test/clang-tidy/check_clang_tidy.py $name/bin
 ln -s /usr/bin/FileCheck-8 $name/bin/FileCheck
-cp clang/tools/extra/clang-tidy/*.h $name/include
-cp clang/LICENSE.TXT $name/clang-LICENSE.TXT
-ln -s /usr/include/clang $name/lib/clang
+# include
+mkdir -p $name/include/{clang-tidy,llvm,clang}
+cp llvm-project/clang-tools-extra/clang-tidy/*.h $name/include/clang-tidy
+ln -s llvm-project/llvm/include $name/include/llvm
+ln -s llvm-project/clang/include $name/include/clang
+# lib
+cp llvm-project/build/lib/clangTidyMain.a $name/lib
+# license
+cp llvm-project/clang/LICENSE.TXT $name/clang-LICENSE.TXT
+cp llvm-project/llvm/LICENSE.TXT $name/llvm-LICENSE.TXT
+cp llvm-project/clang-tools-extra/LICENSE.TXT $name/clang-tools-extra-LICENSE.TXT
 tar -cJvf $name.tar.xz $name
